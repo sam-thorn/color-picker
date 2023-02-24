fbc3a49c8cba287920a3083c14845e8f166daa15
sam-thorn <spthornnz@gmail.com>
2023-02-24 15:07:56 +1300

    Remove comments and update background color based on state

--- src/components/ColorPicker.jsx
+++ src/components/ColorPicker.jsx
@@ -1,37 +1,22 @@
 import React from "react";
-// BG COLOR: use useEffect here to look for changes to a specific thing: changing color codes
 import { HexColorPicker, HexColorInput } from "react-colorful";
 
 function ColorPicker(props) {
-  // const [color, setColor] = useState("#81D274");
-  // useEffect(() => {
-
-  // }
-
   return (
-    <div>
-      <section className="color-picker-layout">
-        <HexColorPicker color={props.color} onChange={props.onChange} />
-        <HexColorInput
-          type="text"
-          color={props.color}
-          onChange={props.onChange}
-        />
-      </section>
-    </div>
+    <>
+      <div>
+        <section className="color-picker-layout">
+          <HexColorPicker color={props.color} onChange={props.onChange} />
+          <HexColorInput
+            id="hex-input"
+            type="text"
+            color={props.color}
+            onChange={props.onChange}
+          />
+        </section>
+      </div>
+    </>
   );
 }
 
 export default ColorPicker;
-
-// When a user changes the hex value in the color picker, the background color changes to the same
-
-// color changes in ColorPicker
-// it needs to be passed in to App as a variable so that the bg color changes
-
-//
-
-// I currently have a color payload that dispatches to the Store
-// A bgcolor reducer has an initialState of a hard-coded color value
-// This initialState can be set to the initial value of the ColorPicker
-//
