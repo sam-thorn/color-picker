791cc66d76504cd5b4d955ce0543d7e8e856d09d
sam-thorn <spthornnz@gmail.com>
2023-03-04 10:58:29 +1300

    Implement feature to copy hex code to clipboard

--- src/components/ColorPicker.jsx
+++ src/components/ColorPicker.jsx
@@ -1,19 +1,46 @@
-import React from "react";
-import { HexColorPicker, HexColorInput } from "react-colorful";
-import CopyButton from "./CopyButton";
+import { TextField, IconButton, InputAdornment } from "@mui/material";
+import React, { useState } from "react";
+import { HexColorPicker } from "react-colorful";
+import { CopyToClipboard } from "react-copy-to-clipboard";
+import ContentCopyIcon from "@mui/icons-material/ContentCopy";
 
 function ColorPicker(props) {
+  const [isCopied, setIsCopied] = useState(false);
+
+  const handleClick = () => {
+    setIsCopied(true);
+    setTimeout(() => {
+      setIsCopied(false);
+    }, 1000);
+  };
+
   return (
     <>
       <section className="color-picker-layout">
         <HexColorPicker color={props.color} onChange={props.onChange} />
-        <HexColorInput
-          id="hex-input"
-          type="text"
-          color={props.color}
-          onChange={props.onChange}
+        <TextField
+          className="text-field"
+          value={props.color.toUpperCase()}
+          variant="outlined"
+          InputProps={{
+            endAdornment: (
+              <InputAdornment position="end">
+                <CopyToClipboard
+                  text={props.color.toUpperCase()}
+                  onClick={handleClick}
+                >
+                  <IconButton
+                    className="copy-button"
+                    variant="contained"
+                    // disableRipple
+                  >
+                    <ContentCopyIcon />
+                  </IconButton>
+                </CopyToClipboard>
+              </InputAdornment>
+            ),
+          }}
         />
-        <CopyButton />
       </section>
     </>
   );
